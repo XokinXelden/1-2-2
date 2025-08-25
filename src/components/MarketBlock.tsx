858e2de6d60f024e03f37c51456215f0fcdd1349
XokinXelden <xokinxelden@gmail.com>
2025-08-25 19:01:51 +0300

    ыы

--- src/components/MarketBlock.tsx
+++ src/components/MarketBlock.tsx
@@ -1,13 +1,13 @@
 
 
 export const MarketBlock = () => {
-  return(
+  return (
     <div className="marketBlock_head">
-          <h4>Марка</h4>
-          <h4>Цена</h4>
-          <h4>Год</h4>
-          <h4>Цвет</h4>
-          <h4>Заказать</h4>
-        </div>
+      <h4>Марка</h4>
+      <h4>Цена</h4>
+      <h4>Год</h4>
+      <h4>Цвет</h4>
+      <h4>Заказать</h4>
+    </div>
   )
 }
